5f43df98c6a0c7c0ca253cad1f564800dec7e4ad
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-30 17:55:25 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,18 +1,20 @@
 class Owner
   # code goes here
  attr_reader :species
+ attr_accessor :name :pets
 
   @@all = []
 
   def initialize()
     @species = "human"
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
   def say_species
     puts "I am a #{species}"
   end
 
-  
+
 
   def save
     @@all << self
@@ -31,5 +33,3 @@ class Owner
   end
 
 end
-
-
